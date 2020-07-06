d780e453de231064bea0d2fabd002b213e699e92
Melissa Lopez <melissa.beth.lopez@gmail.com>
2020-07-06 16:10:25 -0400

    Exercises 3.7-3.8

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+const showRequest = morgan.token('request-body', 
+                      function (req, res) { return JSON.stringify(req.body)})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'))
 
 let persons = [
     {
