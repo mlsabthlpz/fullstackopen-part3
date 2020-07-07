e909394ede928b01ef57fa1c72efc3746ebe355d
Melissa Lopez <melissa.beth.lopez@gmail.com>
2020-07-07 14:03:37 -0400

    setting up Heroku

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 const showRequest = morgan.token('request-body', 
                       function (req, res) { return JSON.stringify(req.body)})
@@ -95,7 +97,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
